19877aa7a20135881d69538e3383de57b2bf0bb3
Abhishek EH <collegewap@gmail.com>
2021-05-25 08:22:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [price, setPrice] = useState();
+  const [autoRefresh, setAutoRefresh] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    const fetchBitcoinPrice = () => {
+      fetch("https://api.coincap.io/v2/assets/bitcoin")
+        .then((response) => response.json())
+        .then((result) => {
+          // Format the price to 2 decimal places
+          const bitcoinPrice = (+result?.data?.priceUsd).toFixed(2);
+          setPrice(bitcoinPrice);
+        })
+        .catch((error) => console.log("error", error));
+    };
+    if (!price) {
+      fetchBitcoinPrice();
+    }
+    if (autoRefresh) {
+      interval = setInterval(() => {
+        fetchBitcoinPrice();
+      }, 5 * 1000);
+    }
+
+    return () => {
+      // Clear the previous interval before the component gets updated
+      clearInterval(interval);
+    };
+  }, [autoRefresh, price]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{price && `Bitcoin Price: $${price}`}</h2>
+      <div className="refresh">
+        <div className="refresh-label">Auto refresh:</div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={autoRefresh}
+            onChange={(e) => {
+              setAutoRefresh(e.target.checked);
+            }}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
     </div>
   );
 }
